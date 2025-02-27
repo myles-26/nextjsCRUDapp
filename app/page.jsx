4c784bd9498dd209b048ea207218652b9d341fa8
myles-26 <mylesj90@gmail.com>
2025-02-27 17:46:57 -0600

    update

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import styles from "./page.module.css";
+import TaskList from "@/components/TaskList";
+
+export default function Home() {
+  
+
+
+  return (
+      <div className={styles.main}>
+        <TaskList />
+      </div>
+  );
+}
